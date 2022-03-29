601075e9dc1493fe0be92ad74a4e6705b4588bbe
dannyoz <daniel.osborne@landg.com>
2022-03-29 20:02:34 +0100

    moved index file

--- api/src/index.js
+++ api/src/index.js
@@ -4,7 +4,9 @@ const app = express();
 app.set("port", process.env.PORT || 5000);
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.json({
+    data: "hello World",
+  });
 });
 
 app.listen(app.get("port"), function () {
