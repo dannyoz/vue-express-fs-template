b7737ef9430293edc24c6de3c98bfa91ed40e110
dannyoz <daniel.osborne@landg.com>
2022-03-29 21:16:42 +0100

    cors set up

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+var corsOptions = {
+  origin: "http://localhost:8080",
+  optionsSuccessStatus: 200,
+};
 
 app.set("port", process.env.PORT || 5000);
 
-app.get("/", (req, res) => {
+app.get("/", cors(corsOptions), (req, res) => {
   res.json({
     data: "hello World",
   });
