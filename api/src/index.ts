cb9b419435a14a2478726c6a5aff9cc4edf6a159
dannyoz <daniel.osborne@landg.com>
2022-03-30 09:31:29 +0100

    routing updates

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { GetRoutes } from "./routes";
-import { CorsConfig } from "./cors";
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,13 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("port", process.env.PORT || 5000);
 
-app.get("/", CorsConfig.allowedRoutes(), (_req: unknown, res: any) => {
-  res.json({
-    data: "hello World",
-  });
-});
-
-new GetRoutes(app).testRoute();
+const getRoutes = new GetRoutes(app);
+getRoutes.home();
+getRoutes.testRoute();
 
 app.listen(app.get("port"), () => {
   // tslint:disable-next-line
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,6 +3,18 @@ import { CorsConfig } from "./cors";
 export class GetRoutes {
   constructor(public app: any) {}
 
+  home() {
+    return this.app.get(
+      "/",
+      CorsConfig.allowedRoutes(),
+      (_req: unknown, res: any) => {
+        res.json({
+          data: "hello World",
+        });
+      }
+    );
+  }
+
   testRoute() {
     return this.app.get(
       "/test",
