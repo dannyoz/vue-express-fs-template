067ee0969a244649275a36c88e98b3f07cec3c5b
dannyoz <daniel.osborne@landg.com>
2022-03-30 10:35:13 +0100

    routing configuration

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,29 +1,22 @@
 import { CorsConfig } from "./cors";
 
 export class GetRoutes {
+  cors = CorsConfig.allowedRoutes();
   constructor(public app: any) {}
 
   home() {
-    return this.app.get(
-      "/",
-      CorsConfig.allowedRoutes(),
-      (_req: unknown, res: any) => {
-        res.json({
-          data: "hello World",
-        });
-      }
-    );
+    return this.app.get("/", this.cors, (_req: unknown, res: any) => {
+      res.json({
+        data: "hello World",
+      });
+    });
   }
 
   testRoute() {
-    return this.app.get(
-      "/test",
-      CorsConfig.allowedRoutes(),
-      (_req: unknown, res: any) => {
-        res.json({
-          data: "test",
-        });
-      }
-    );
+    return this.app.get("/test", this.cors, (_req: unknown, res: any) => {
+      res.json({
+        data: "test",
+      });
+    });
   }
 }
